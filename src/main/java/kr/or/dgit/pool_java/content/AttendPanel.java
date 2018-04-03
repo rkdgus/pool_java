32b639fecd4668da5f5152ef95c38270a7e770e9
HongBumHa <goldstate@naver.com>
2018-04-03 18:08:20 +0900

    엑셀 + 출석부 80% 완성

--- src/main/java/kr/or/dgit/pool_java/content/AttendPanel.java
+++ src/main/java/kr/or/dgit/pool_java/content/AttendPanel.java
@@ -42,7 +42,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 
-
 public class AttendPanel extends JPanel {
 	private JTable table;
 	private AttendanceDao aDao;
@@ -56,7 +55,8 @@ public class AttendPanel extends JPanel {
 
 	/**
 	 * Create the panel.
-	 * @throws ParseException 
+	 * 
+	 * @throws ParseException
 	 */
 	public AttendPanel() throws ParseException {
 		setLayout(null);
@@ -66,10 +66,10 @@ public class AttendPanel extends JPanel {
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(12, 99, 878, 435);
 		add(scrollPane);
-		
+
 		table = new JTable();
 		scrollPane.setViewportView(table);
-		
+
 		JButton btnNewButton = new JButton("프린트");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -78,83 +78,75 @@ public class AttendPanel extends JPanel {
 		});
 		btnNewButton.setBounds(774, 10, 97, 30);
 		add(btnNewButton);
-		
 
 		List<Register> lists = RegisterService.getInstance().selectByCno(6);
 		loadDataPrice(lists, 4, 2018);
-		
-		
 
-		
 		JComboBox yearBox = new JComboBox();
-		
+
 		yearBox.setBounds(54, 11, 81, 30);
 		add(yearBox);
 		Date d = new Date();
 		SimpleDateFormat sf = new SimpleDateFormat("yyyy");
 		SimpleDateFormat sf1 = new SimpleDateFormat("MM");
-		int year=Integer.parseInt(sf.format(d));
-		int month=Integer.parseInt(sf1.format(d));
-		
-		for(int i = 2017;i<=year;i++) {
+		int year = Integer.parseInt(sf.format(d));
+		int month = Integer.parseInt(sf1.format(d));
+
+		for (int i = 2017; i <= year; i++) {
 			yearBox.addItem(i);
 		}
 		yearBox.setSelectedItem(year);
 		JComboBox monthBox = new JComboBox();
-		
+
 		monthBox.setBounds(173, 11, 54, 30);
 		add(monthBox);
-		String mm="";
-		for(int i = 1;i<=12;i++) {
-			if(i<10) {
-				mm="0"+i;
-			}else {
-				mm=i+"";
+		String mm = "";
+		for (int i = 1; i <= 12; i++) {
+			if (i < 10) {
+				mm = "0" + i;
+			} else {
+				mm = i + "";
 			}
 			monthBox.addItem(mm);
 		}
 		monthBox.setSelectedItem(sf1.format(d));
-		
-	
-		
-		
+
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(274, 10, 160, 30);
 		add(comboBox);
-		
+
 		JLabel lblNewLabel = new JLabel("년");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setBounds(135, 10, 39, 38);
 		add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("월");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setBounds(224, 10, 39, 38);
 		add(lblNewLabel_1);
-		
+
 		JLabel title = new JLabel("");
 		title.setFont(new Font("굴림", Font.BOLD, 17));
 		title.setHorizontalAlignment(SwingConstants.CENTER);
 		title.setBounds(303, 50, 259, 39);
 		add(title);
 		title.setText("출석부");
-		
-		List<Class> items = cDao.selectByTime(yearBox.getSelectedItem()+"-"+monthBox.getSelectedItem()+"-01");
-		
-		
-		for(Class c:items) {
+
+		List<Class> items = cDao.selectByTime(yearBox.getSelectedItem() + "-" + monthBox.getSelectedItem() + "-01");
+
+		for (Class c : items) {
 			comboBox.addItem(c.toString());
 		}
-		
-		
+
 		yearBox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				comboBox.removeAllItems();
-				List<Class> items = cDao.selectByTime(yearBox.getSelectedItem()+"-"+monthBox.getSelectedItem()+"-01");
-				if(items.size()==0) {
+				List<Class> items = cDao
+						.selectByTime(yearBox.getSelectedItem() + "-" + monthBox.getSelectedItem() + "-01");
+				if (items.size() == 0) {
 					return;
 				}
-				for(Class c:items) {
+				for (Class c : items) {
 					comboBox.addItem(c.toString());
 				}
 			}
@@ -162,164 +154,172 @@ public class AttendPanel extends JPanel {
 		monthBox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				comboBox.removeAllItems();
-				List<Class> items = cDao.selectByTime(yearBox.getSelectedItem()+"-"+monthBox.getSelectedItem()+"-01");
-				if(items.size()==0) {
+				List<Class> items = cDao
+						.selectByTime(yearBox.getSelectedItem() + "-" + monthBox.getSelectedItem() + "-01");
+				if (items.size() == 0) {
 					return;
 				}
-				for(Class c:items) {
+				for (Class c : items) {
 					comboBox.addItem(c.toString());
 				}
 			}
 		});
-		
+
 		comboBox.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(comboBox.getSelectedItem() == null){
+				if (comboBox.getSelectedItem() == null) {
 					return;
 				}
-				String str = comboBox.getSelectedItem()+"";
+				String str = comboBox.getSelectedItem() + "";
 				System.out.println(str);
-				String[] arr =str.split("/");
-				System.out.println("arr" +arr);
+				String[] arr = str.split("/");
+				System.out.println("arr" + arr);
 				str = arr[0];
-				String str1 =arr[1];
-				
+				String str1 = arr[1];
+
 				List<Register> lists = rDao.selectByCno(Integer.parseInt(str));
 				try {
-					loadDataPrice(lists, Integer.parseInt(monthBox.getSelectedItem()+""), (int)yearBox.getSelectedItem());
+					loadDataPrice(lists, Integer.parseInt(monthBox.getSelectedItem() + ""),
+							(int) yearBox.getSelectedItem());
 				} catch (ParseException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				
+
 				setTable();
-				title.setText("["+str1+"] 출석부");
+				title.setText("[" + str1 + "] 출석부");
 			}
 		});
 
-	
 	}
-	
-	public void loadDataPrice(List<Register> lists,int month,int year) throws ParseException {
+
+	public void loadDataPrice(List<Register> lists, int month, int year) throws ParseException {
 		DefaultTableModel model = new DefaultTableModel(getDataPrice(lists, month, year), getColumnNames(month));
 		table.setModel(model);
-		
+
 	}
-	
+
 	protected Object[] getColumnNames(int month) {
-		int[] day= {32,29,32,31,32,31,32,32,31,32,31,32};
-		String[] colum =new String[day[month]];
-		for(int i=0;i<colum.length;i++) {
-			if(i==0) {
+		int[] day = { 32, 29, 32, 31, 32, 31, 32, 32, 31, 32, 31, 32 };
+		String[] colum = new String[day[month]];
+		for (int i = 0; i < colum.length; i++) {
+			if (i == 0) {
 				colum[i] = "이름";
-			}else {
-				colum[i] = i+"";
+			} else {
+				colum[i] = i + "";
 			}
-			
+
 		}
 		return colum;
 	}
+
 	public void setTable() {
 		DefaultTableCellRenderer tScheduleCellRenderer = new DefaultTableCellRenderer();
 
-
 		tScheduleCellRenderer.setHorizontalAlignment(SwingConstants.CENTER);
-		 
-		table.getColumnModel().getColumn(0).setMaxWidth(50); 
-        table.getColumnModel().getColumn(0).setMinWidth(50); 
-        table.getColumnModel().getColumn(0).setWidth(50); 
-        for(int i=0;i<table.getColumnCount();i++) {
-        	table.getColumnModel().getColumn(i).setCellRenderer(tScheduleCellRenderer);
-        }
+
+		table.getColumnModel().getColumn(0).setMaxWidth(50);
+		table.getColumnModel().getColumn(0).setMinWidth(50);
+		table.getColumnModel().getColumn(0).setWidth(50);
+		for (int i = 0; i < table.getColumnCount(); i++) {
+			table.getColumnModel().getColumn(i).setCellRenderer(tScheduleCellRenderer);
+		}
 	}
-	
-	
-	protected Object[][] getDataPrice(List<Register> lists,int month,int year) throws ParseException {
+
+	protected Object[][] getDataPrice(List<Register> lists, int month, int year) throws ParseException {
 		SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
-		
-		int[] day= {32,29,32,31,32,31,32,32,31,32,31,32};
-		String[] colum =new String[day[month]];
+
+		int[] day = { 32, 29, 32, 31, 32, 31, 32, 32, 31, 32, 31, 32 };
+		String[] colum = new String[day[month]];
 		Object[][] data = new Object[lists.size()][];
-		Attendance attendance =null;
-		List<Attendance> list =null ;
+		Attendance attendance = null;
+		List<Attendance> list = null;
 		String m = "";
 		String s = "";
-		HashMap<String, Object> map= new HashMap<>();
-		
-		if(month<10) {
-			m = "0"+month;
-		}else {
-			m=month+"";
+		HashMap<String, Object> map = new HashMap<>();
+
+		if (month < 10) {
+			m = "0" + month;
+		} else {
+			m = month + "";
 		}
 		for (int i = 0; i < lists.size(); i++) {
 			Object[] d = new Object[day[month]];
-			for(int j=0;j<colum.length;j++) {
-				if(j==0) {
+			for (int j = 0; j < colum.length; j++) {
+				if (j == 0) {
 					d[j] = MemberService.getInstance().selectMno(lists.get(i).getMno()).getName();
-				}else {
-					
-					d[j]="O";
+				} else {
+					if (j < 10) {
+						s = "0" + j;
+					} else {
+						s = j + "";
+					}
+
+					map.put("date", year + "-" + m + "-" + s);
+					map.put("mno", lists.get(i).getMno());
+					list = aDao.selectDate(map);
+					if (list.size() != 0) {
+						d[j] = "O";
+					}
+
 				}
 			}
-			
+
 			data[i] = d;
 
 		}
 		return data;
 	}
-	
 
 	private void excelFile() {
 		String[] getColumn = new String[table.getColumnCount()];
-		for(int i = 0 ; i < table.getColumnCount(); i ++) {
+		for (int i = 0; i < table.getColumnCount(); i++) {
 			getColumn[i] = table.getColumnName(i);
 		}
-		
-		
+
 		String[][] getData = new String[table.getRowCount()][table.getColumnCount()];
-		
-		for(int i = 0; i < table.getRowCount(); i ++) {
-			for(int j = 0; j < table.getColumnCount(); j++) {
-				System.out.println((String)table.getValueAt(i,j));
-				getData[i][j] = (String)table.getValueAt(i,j);
+
+		for (int i = 0; i < table.getRowCount(); i++) {
+			for (int j = 0; j < table.getColumnCount(); j++) {
+				System.out.println((String) table.getValueAt(i, j));
+				getData[i][j] = (String) table.getValueAt(i, j);
 			}
 		}
-		
+
 		try {
 			File file = new File(FilePath2);
-			if(!file.exists()) {
+			if (!file.exists()) {
 				System.out.println();
 				file.mkdir();
 			}
 			File file1 = new File(FilePath);
-			
-			if(!file1.exists()) {
+
+			if (!file1.exists()) {
 				file1.createNewFile();
 			}
 			WritableWorkbook workbook = Workbook.createWorkbook(file1);
-			WritableSheet sheet = workbook.createSheet(SheetName,0);
-			
+			WritableSheet sheet = workbook.createSheet(SheetName, 0);
+
 			WritableCellFormat format_column = new WritableCellFormat();
 			WritableCellFormat format_data = new WritableCellFormat();
-			WritableCellFormat format_integer1 = new WritableCellFormat(NumberFormats.INTEGER);
 			format_column.setBackground(jxl.format.Colour.GRAY_25);
-			format_column.setBorder(jxl.format.Border.ALL,jxl.format.BorderLineStyle.THIN);
-			for(int i =0; i < getColumn.length; i++) {
-				Label label = new Label(i,0,getColumn[i],format_column);
+			format_column.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN);
+			for (int i = 0; i < getColumn.length; i++) {
+				Label label = new Label(i, 0, getColumn[i], format_column);
 				sheet.addCell(label);
 			}
-			
-			for(int i = 0; i<getData.length; i++) {
-				for(int j = 0; j < getData[i].length; j++) {
-					Label label = new Label(j,i+1,getData[i][j],format_data);
+
+			for (int i = 0; i < getData.length; i++) {
+				for (int j = 0; j < getData[i].length; j++) {
+					Label label = new Label(j, i + 1, getData[i][j], format_data);
 					sheet.addCell(label);
 				}
 			}
 			workbook.write();
 			workbook.close();
-		}catch(Exception e1) {
+		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
 	}
