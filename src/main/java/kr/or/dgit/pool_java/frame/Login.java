0168dc89a548db6e1bbb0ed071693f151618c93d
KangHyun Lee <rkd7327@naver.com>
2018-04-12 10:51:40 +0900

    s

--- src/main/java/kr/or/dgit/pool_java/frame/Login.java
+++ src/main/java/kr/or/dgit/pool_java/frame/Login.java
@@ -1,41 +1,30 @@
 package kr.or.dgit.pool_java.frame;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.Frame;
+import java.awt.Font;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
 
 import org.jdesktop.xswingx.PromptSupport;
 
-
 import kr.or.dgit.pool_java.dao.TeacherDao;
 import kr.or.dgit.pool_java.dto.Teacher;
 import kr.or.dgit.pool_java.service.TeacherService;
-import java.awt.Font;
-import java.awt.Dialog.ModalExclusionType;
 
 public class Login extends JFrame {
 
